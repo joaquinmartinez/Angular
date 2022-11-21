a75dc5482afcbbf5a1d2dff7bc739de954ef5d0a
Joaquin Martinez <joaquinmartinez7070@gmail.com>
2022-11-21 10:57:24 -0300

    Se actualizaron cambios en la lista de productos

--- bohemian/src/app/product-list/product-list.component.ts
+++ bohemian/src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './Product';
 
 @Component({
   selector: 'app-product-list',
@@ -6,38 +7,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-  product = {
-        "image": "assets/images/CervezaAntaresHoney.png",
-        "name": "Cerveza",
-        "type": "Honey",
-        "characteristic": "Rubia",
-        "price": 200,
-        "stock": 1500
-      }
-  product2 = {
-        "image": "assets/images/CervezaAntaresHoney.png",
-        "name": "Cerveza",
-        "type": "Scotch",
-        "characteristic": "Rubia/maltosa",
-        "price": 210,
-        "stock": 2300
-      }
-  product3 = {
-    "image": "assets/images/CervezaAntaresHoney.png",
-    "name": "Cerveza",
-    "type": "Kolsch",
-    "characteristic": "Dorada/fresca/suave",
-    "price": 250,
-    "stock": 1900
-  }
-  product4 = {
-    "image": "assets/images/CervezaAntaresHoney.png",
-    "name": "Cerveza",
-    "type": "Stout",
-    "characteristic": "Negra/tostada/cremosa",
-    "price": 270,
-    "stock": 3000
-  }
+  products: Product[] = [
+    
+    {
+      image: "assets/images/EspumanteDada.png",
+      name: "Vino",
+      type: "Espumante",
+      characteristic: "Blanco",
+      price: 420,
+      stock: 3200,
+      clearance: false,
+    },
+    {
+      image: "assets/images/MalbecRutini.png",
+      name: "Vino",
+      type: "Malbec",
+      characteristic: "Tinto",
+      price: 420,
+      stock: 0,
+      clearance: false,
+    },
+    {
+      image: "assets/images/CervezaAntaresStout.png",
+      name: "Cerveza",
+      type: "Stout",
+      characteristic: "Negra/tostada/cremosa",
+      price: 270,
+      stock: 3000,
+      clearance: true,
+    },
+    {
+      image: "assets/images/CervezaAntaresScotch.png",
+      name: "Cerveza",
+      type: "Scotch",
+      characteristic: "Rubia/maltosa",
+      price: 210,
+      stock: 0,
+      clearance: false,
+    },
+    {
+      image: "assets/images/CervezaAntaresKolsch.png",
+      name: "Cerveza",
+      type: "Kolsch",
+      characteristic: "Dorada/fresca/suave",
+      price: 250,
+      stock: 1900,
+      clearance: false,
+    },
+    {
+      image: "assets/images/CervezaAntaresHoney.png",
+      name: "Cerveza",
+      type: "Honey",
+      characteristic: "Rubia",
+      price: 200,
+      stock: 1500,
+      clearance: true,
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
